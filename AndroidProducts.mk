02d349adc121f4c827232e3e23976d89b71a4540
RobertGarciaa <chae0218@naver.com>
2025-11-21 19:43:26 +0900

    renoir: PixelOS16
    Signed-off-by: RobertGarciaa <chae0218@naver.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -4,4 +4,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_renoir.mk
+    $(LOCAL_DIR)/custom_renoir.mk
--- custom_renoir.mk
+++ custom_renoir.mk
@@ -6,14 +6,14 @@
 # Inherit from renoir device
 $(call inherit-product, device/xiaomi/renoir/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common PixelOS stuff.
+$(call inherit-product, vendor/custom/config/common_full_phone.mk)
 
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := renoir
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := M2101K9G
-PRODUCT_NAME := lineage_renoir
+PRODUCT_NAME := custom_renoir
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="renoir_global-user 13 TKQ1.220829.002 V14.0.7.0.TKIMIXM release-keys" \
